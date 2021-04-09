a07655fc2cc7c066bf48426c9072af8ec4653d79
Joko <joiel@users.noreply.github.com>
2021-04-09 15:13:46 +0700

    minor fix

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import * as config from 'config';
 
 import { CompaniesService } from './services/companies.service';
 import { CompaniesController } from './controllers/companies.controller';
-import { ContactsRepository } from 'src/contacts/repositories/contacts.repository';
 import { LogsModule } from 'src/logs/logs.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Contacts, ContactsSchema } from 'src/contacts/schemas/contacts.schema';
 const jwt = config.get('jwt');
 
 @Module({
@@ -20,7 +20,9 @@ const jwt = config.get('jwt');
         expiresIn: jwt.expires,
       },
     }),
-    TypeOrmModule.forFeature([ContactsRepository]),
+    MongooseModule.forFeature([
+      { name: Contacts.name, schema: ContactsSchema },
+    ]),
   ],
   providers: [CompaniesService],
   controllers: [CompaniesController],
