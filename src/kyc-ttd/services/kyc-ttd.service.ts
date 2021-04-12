58b6d82e2f138b07ffe0c3caaa0818d3a662b027
Joko <joiel@users.noreply.github.com>
2021-04-12 14:30:38 +0700

    kyc fixing

--- src/kyc-ttd/services/kyc-ttd.service.ts
+++ src/kyc-ttd/services/kyc-ttd.service.ts
@@ -3,18 +3,18 @@ import {
   HttpService,
   HttpStatus,
   Injectable,
-  NotFoundException,
 } from '@nestjs/common';
 import { BaseResponse } from 'src/utils/base/response/base.response';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import * as dayjs from 'dayjs';
+
 import {
   KYC as KYCTTD,
   KYCDocument as KYCTTDDocument,
 } from 'src/utils/base/schema/kyc.schema';
 import { LogsService } from 'src/logs/services/logs.service';
-import { KycSpecimentDto } from '../dtos/kyc-ttd.create.dto';
+import { KycSpecimentDto } from '../dtos/kyc-ttd.speciment.dto';
 import * as config from 'config';
 import {
   Settings,
@@ -43,7 +43,8 @@ export class KYCTTDService {
       const kycConfig = config.get('kyc');
       const sendingSpeciment = await this.httpService
         .post(
-          kycConfig.URL + '/gateway/jwtSandbox/1.0/getJsonWebToken/v1',
+          kycConfig.URL +
+            '/gateway/digitalSignatureFullJwtSandbox/1.0/sendSpeciment/v1',
           {
             param: {
               email: req.user.email,
@@ -60,13 +61,66 @@ export class KYCTTDService {
           },
         )
         .toPromise();
-
+      if (sendingSpeciment) {
+        this.logsService.create({
+          user_id: req.user.id,
+          activity: 'success',
+          content: req.ip,
+          module: 'login',
+        });
+      }
+      return new BaseResponse<any>(
+        HttpStatus.ACCEPTED,
+        'SUCCESS',
+        'Settings found',
+        {},
+      );
+    } catch (error) {
       this.logsService.create({
         user_id: req.user.id,
-        activity: 'success',
-        content: req.ip,
+        activity: 'failed',
+        content: error.message + ' with ip : ' + req.ip,
         module: 'login',
       });
+      throw new BadRequestException(
+        `Anda mengalami error: ${error.message}. Hubungi Admin untuk bantuan`,
+      );
+    }
+  }
+
+  async checkCertificate(req): Promise<BaseResponse<any>> {
+    try {
+      const peruriToken = await this.settingsRepository.findOne({
+        key: 'peruri_token',
+      });
+      const kycConfig = config.get('kyc');
+      const checkCertificate = await this.httpService
+        .post(
+          kycConfig.URL +
+            '/gateway/digitalSignatureFullJwtSandbox/1.0/checkCertificate/v1',
+          {
+            param: {
+              email: req.user.email,
+              systemId: kycConfig.SYSTEM_ID,
+            },
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'x-Gateway-APIKey': kycConfig.API_KEY,
+              Authorization: 'Bearer ' + peruriToken.value,
+            },
+          },
+        )
+        .toPromise();
+      if (checkCertificate) {
+        this.logsService.create({
+          user_id: req.user.id,
+          activity: 'success',
+          content: req.ip,
+          module: 'login',
+        });
+      }
       return new BaseResponse<any>(
         HttpStatus.ACCEPTED,
         'SUCCESS',
